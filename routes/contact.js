2c17903f3ec3a5a081f14334b6fd9d779471abf6
Chuileee <leechui618@gmail.com>
2023-06-13 23:30:24 -0400

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -8,15 +8,26 @@ router.get('/', function(req, res, next) {
 
 /* POST contact form submission. */
 router.post('/', function(req, res, next) {
+    // Extract form fields
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phoneNumber = req.body.phoneNumber;
+    const email = req.body.email;
+    const message = req.body.message;
+
     // Handle the form submission here
-    console.log(req.body);
-    
+    console.log("First Name: ", firstName);
+    console.log("Last Name: ", lastName);
+    console.log("Phone Number: ", phoneNumber);
+    console.log("Email: ", email);
+    console.log("Message: ", message);
+
     // After you've processed the form data, render the confirmation page
     // Notice the additional 'title' property in the object
     res.render('confirmation', { 
       title: 'Confirmation', 
       message: 'Thanks for your message! We will get back to you soon.' 
     });
-  });
+});
 
 module.exports = router;
